ec0f3e48913c6d20133342b1291f3a241eb4fb07
yrkrus <137649829+yrkrus@users.noreply.github.com>
2024-04-02 15:52:24 +0300

    Add files via upload
    проверка на кратность

---
+++ IsKPeriodic.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+
+using std::cout;
+using std::cin;
+
+bool IsKPeriodic(const std::string &str, int kratnost) {
+    
+    int lenght = str.length();
+
+    if (lenght % kratnost != 0) {
+        return true;
+    }
+
+    for (int i = 0; i < lenght - kratnost; ++i)
+    {
+        if (str[i] != str[i % kratnost])
+        {
+            return false; 
+        }
+    }
+
+    return true;        
+}
+
+
+int main()
+{    
+    setlocale(LC_ALL, "");
+   
+    // ввод строки
+    std::string stroka;
+    cout << "ввод строки \n >> ";
+    std::getline(cin, stroka);
+
+   
+    cout << "\n\nввод крастности \n >> ";
+    unsigned short kratnost{ 0 };
+
+    cin >> kratnost;
+    if (!kratnost || kratnost == 0) {
+        cout << "разрешен ввод только целочисленной кратности отличной от 0";
+        return -1;
+    }
+
+    (IsKPeriodic(stroka, kratnost)) ? cout << "Строка кратна " << kratnost : cout << "Строка не кратна " << kratnost;
+            
+    return 0;
+
+}
+
